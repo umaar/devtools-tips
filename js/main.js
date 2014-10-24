fe415631bce7606cbc2e369e5ee1bae7f805999f
Umar <umar.hansa@gmail.com>
2014-10-24 15:27:42 +0100

    parallel ajax requests

--- js/main.js
+++ js/main.js
@@ -11,6 +11,38 @@ $(function() {
 		"console"
 	];
 
+	var videos;
+
+	var videoPlayback = {
+		stop: function(vid) {
+			vid.currentTime = 0;
+		},
+		play: function(vid) {
+			vid.play();
+		}
+	};
+
+	function toggleVideos(state) {
+		videos.each(function() {
+			videoPlayback[state]();
+		});
+	}
+
+	function bindControls() {
+		$('.settings-options-play-all').on('click', function() {
+			toggleVideos('play');
+		});
+
+		$('.settings-options-stop-all').on('click', function() {
+			toggleVideos('stop');
+		});
+	}
+
+	function tipsReady(data) {
+		videos = $('video');
+		bindControls();
+	}
+
 	function group(data) {
 		var grouped = [];
 		data.forEach(function(tip) {
@@ -30,20 +62,24 @@ $(function() {
 	}
 
 	function filterData(data) {
-		render(group(data));
+		return group(data);
 	}
 
 	function getMedia() {
 		return $.getJSON( "media/media.json");
 	}
 
-	function render(data) {
-		console.log(data);
-		$.get('tip.mustache', function(template) {
-			var rendered = Mustache.render(template, {data: data});
-			$('.tips').append(rendered);
-		});
+	function getTemplate(data) {
+		return $.get('tip.mustache');
 	}
 
-	getMedia().done(filterData);
+	function render(data, template) {
+		var rendered = Mustache.render(template, {data: filterData(data)});
+		$('.tips').append(rendered);
+	}
+
+	$.when(getMedia(), getTemplate()).done(function(data, template) {
+		render(data[0], template[0]);
+		tipsReady();
+	});
 });
