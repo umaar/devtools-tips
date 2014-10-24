958aff2af4e9d8df49e535b429633db44a4c66cb
Umar <umar.hansa@gmail.com>
2014-10-24 14:42:19 +0100

    Group by panel

--- js/main.js
+++ js/main.js
@@ -1,16 +1,49 @@
 $(function() {
 
+	var panels = [
+		"elements",
+		"network",
+		"sources",
+		"timeline",
+		"profiles",
+		"resources",
+		"audits",
+		"console"
+	];
+
+	function group(data) {
+		var grouped = [];
+		data.forEach(function(tip) {
+			var panel = tip.panel;
+
+			var order = panels.indexOf(panel);
+			if (grouped[order] === undefined) {
+				grouped[order] = {
+					panel: panel,
+					tips: []
+				};
+			}
+			grouped[order].tips.push(tip);
+		});
+
+		return grouped;
+	}
+
+	function filterData(data) {
+		render(group(data));
+	}
+
 	function getMedia() {
 		return $.getJSON( "media/media.json");
 	}
 
 	function render(data) {
+		console.log(data);
 		$.get('tip.mustache', function(template) {
-			var rendered = Mustache.render(template, {tips: data});
+			var rendered = Mustache.render(template, {data: data});
 			$('.tips').append(rendered);
 		});
 	}
 
-	getMedia().done(render);
-
+	getMedia().done(filterData);
 });
